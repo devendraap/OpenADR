d6401f00f220e40bb497f44278df49d76df1cede
Ubuntu <ubuntu@ip-172-31-70-52.ec2.internal>
2020-06-07 01:38:53 +0000

    logs

---
+++ OpenADRServerVTN20b/src/main/resources/public/precache-manifest.0611b83ab1b0a192604211e8a027ca95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f3b806a8e985cd984999b36f6b69e1",
+    "url": "/testvtn/index.html"
+  },
+  {
+    "revision": "3abfa47ab01008fe811e",
+    "url": "/testvtn/static/css/2.c1f9cd9a.chunk.css"
+  },
+  {
+    "revision": "8c70b5460ee6dc38424e",
+    "url": "/testvtn/static/css/main.41c80ac9.chunk.css"
+  },
+  {
+    "revision": "3abfa47ab01008fe811e",
+    "url": "/testvtn/static/js/2.2c1b5629.chunk.js"
+  },
+  {
+    "revision": "8c70b5460ee6dc38424e",
+    "url": "/testvtn/static/js/main.b93d0174.chunk.js"
+  },
+  {
+    "revision": "f40e7e2bd9c820b270a9",
+    "url": "/testvtn/static/js/runtime-main.f4edcb95.js"
+  }
+]);
---
+++ OpenADRServerVTN20b/src/main/resources/public/precache-manifest.2ff89b15ed83c423ddcfd261f7483648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244941a6a4e6d3c72da29b5d2f635d3e",
+    "url": "/testvtn/index.html"
+  },
+  {
+    "revision": "3abfa47ab01008fe811e",
+    "url": "/testvtn/static/css/2.c1f9cd9a.chunk.css"
+  },
+  {
+    "revision": "54c6036564e9ae9d986b",
+    "url": "/testvtn/static/css/main.41c80ac9.chunk.css"
+  },
+  {
+    "revision": "3abfa47ab01008fe811e",
+    "url": "/testvtn/static/js/2.2c1b5629.chunk.js"
+  },
+  {
+    "revision": "54c6036564e9ae9d986b",
+    "url": "/testvtn/static/js/main.d64ef815.chunk.js"
+  },
+  {
+    "revision": "f40e7e2bd9c820b270a9",
+    "url": "/testvtn/static/js/runtime-main.f4edcb95.js"
+  }
+]);
